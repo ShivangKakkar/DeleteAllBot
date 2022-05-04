9ba370996e07ff23716e7e1e566ae01b24b44429
Stark Programmer <88478059+StarkBotsIndustries@users.noreply.github.com>
2022-05-04 14:02:14 +0530

    Add database and Update PyStark

--- bot.py
+++ bot.py
@@ -1,17 +1,8 @@
-from pyrogram import idle
-from pystark import Stark, plugins
+from pystark import Stark
 from userbot import userbot
 
 
 if __name__ == "__main__":
     bot = Stark()
     userbot.start()
-    bot.start()
-    bot.load_modules('plugins')
-    bot.load_modules(plugins.__path__[0])
-    directory = plugins.__path__[0]
-    Stark.log(f"@{bot.get_me()['username']} is now running...")
-    idle()
-    bot.stop()
-    userbot.stop()
-    Stark.log("Bot has stopped working")
+    bot.activate()
